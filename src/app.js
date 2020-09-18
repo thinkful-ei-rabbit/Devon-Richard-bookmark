82fc6c4c85e2adc62ae355e33f00da3ce9b16956
Devon Reihl <devon.reihl@gmail.com>
2020-09-18 10:40:41 -0700

    added validation

--- src/app.js
+++ src/app.js
@@ -48,6 +48,22 @@ app.get('/bookmark/:id', (req, res) => {
 
 app.post('/bookmark', (req, res) => {
   const newbookMark = req.body;
+
+  const required = [ 'title', 'url' , 'rating'];
+  for( let key of required ) {
+      if( !req.body[key] ) {
+        return res.status(400).send(`Missing ${key} in request`);
+      }
+  }
+
+  if(!url.startsWith("http")) {
+    return res.status(400).send('URL must include "http"')
+  }
+
+  if(isNaN(rating)) {
+    return res.status(400).send('Rating must be a numeric value')
+  }
+
   newbookMark.id = uuidv4();
   bookmarkData.push(newbookMark);
   res
@@ -55,7 +71,5 @@ app.post('/bookmark', (req, res) => {
     .location(`http://localhost:8000/bookmark/${newbookMark.id}`)
     .json(newbookMark);
 });
-
-module.exports = app;
 
 module.exports = app;
